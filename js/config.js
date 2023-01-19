79d778e6fd56c441f02427b0fee9cfcea22119a7
wladimir roman <wladimirroman@hotmail.com>
2023-01-19 20:54:22 -0300

    version 1

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_OcZ3yohKH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2ivc7grtkd20gspttrp4dstn6n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://b283vvg8x4.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
